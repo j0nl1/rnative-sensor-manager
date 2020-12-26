ee4f1a719695ab0118b5ad62a0d31c9d2cd92d2d
j0nl1 <jonlitech@gmail.com>
2020-12-26 17:57:35 +0100

    Add tsconfig in example project and remove index.js

--- example/babel.config.js
+++ example/babel.config.js
@@ -1,14 +1,14 @@
-const path = require('path');
-const pak = require('../package.json');
+const path = require("path");
+const pak = require("../package.json");
 
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+  presets: ["module:metro-react-native-babel-preset"],
   plugins: [
     [
-      'module-resolver',
+      "module-resolver",
       {
         alias: {
-          [pak.name]: path.join(__dirname, '..', pak.source),
+          [pak.name]: path.join(__dirname, "..", pak["react-native"]),
         },
       },
     ],
